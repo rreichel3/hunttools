e5f49bcc8eb805da1622a6795f0603dac366c08a
Robert Reichel <rreichel3@github.com>
2021-03-15 17:13:20 -0400

    Added dump redis go code

---
+++ cmd/dump_redis.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"fmt"
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	redisDumpCmd.Flags().StringVarP(&RedisHostname, "host", "", "localhost", "Redis hostname. Defaults to locahost")
+	redisDumpCmd.Flags().IntVarP(&RedisPort, "port", "p", 6379, "Redis Port. Defaults to 6379")
+	redisDumpCmd.Flags().StringVarP(&RedisPassword, "password", "s", "", "Redis password. Defaults to empty.")
+	redisDumpCmd.Flags().IntVarP(&MaxDump, "count", "n", -1, "Max number to dump. Defaults to all. May dump a few more depending on how much comes from redis.")
+	redisDumpCmd.Flags().BoolVarP(&DumpJson, "json", "j", false, "Dump as json")
+	rootCmd.AddCommand(redisDumpCmd)
+}
+var ctx = context.Background()
+
+var RedisHostname string
+var RedisPort int
+var RedisPassword string
+var MaxDump int 
+var DumpJson bool
+var redisDumpCmd = &cobra.Command{
+	Use:   "redis-dump",
+	Short: "Dump all keys and values in Redis.. Safely",
+	Long:  `Dump all keys and values in Redis.. Safely. Uses SCAN.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     fmt.Sprintf("%s:%d", RedisHostname, RedisPort),
+			Password: RedisPassword, // no password set
+			DB:       0,  // use default DB
+		})
+		var cursor uint64
+		var n int
+		var allKeys = make(map[string]string)
+		for {
+			var keys []string
+			var err error
+			keys, cursor, err = rdb.Scan(ctx, cursor, "*", 10).Result()
+			if err != nil {
+				panic(err)
+			}
+			for _, key := range keys {
+				allKeys[key] = ""
+			}
+			n += len(keys)
+			if cursor == 0 || (MaxDump != -1 && n >= MaxDump) {
+				break
+			}
+		}
+		if VerboseOutput {
+			fmt.Printf("found %d keys\n", n)
+		}
+		idx := 1
+		for key, _ := range allKeys {
+			val, err := rdb.Get(ctx, key).Result()
+			if err != nil {
+				fmt.Printf("Error encountered getting key: %s\n", key)
+				continue
+			}
+			allKeys[key] = val
+			idx++
+		}
+		idx = 1
+		if DumpJson {
+			jsonStr, _ := json.Marshal(allKeys)
+			fmt.Println(string(jsonStr))
+		} else {
+			for k, v := range allKeys {
+				if VerboseOutput {
+					fmt.Printf("%d: %s => %s\n", idx, k, v)
+				} else {
+					fmt.Printf("%s => %s\n", k, v)
+				}
+				idx++
+			}
+		}
+		
+
+		return nil
+
+	},
+}
+
