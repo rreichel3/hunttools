91fb912e461ac9f20ed30f8552a00ff2dcfeddf8
Robert Reichel <rreichel3@github.com>
2021-03-22 10:37:55 -0400

    Working build

--- cmd/redis.go
+++ cmd/redis.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,17 +10,18 @@ func init() {
 	redisRootCmd.Flags().StringVarP(&RedisHostname, "host", "H", "localhost", "Redis hostname. Defaults to locahost")
 	redisRootCmd.Flags().IntVarP(&RedisPort, "port", "P", 6379, "Redis Port. Defaults to 6379")
 	redisRootCmd.Flags().StringVarP(&RedisPassword, "password", "p", "", "Redis password. Defaults to empty.")
+	redisRootCmd.Flags().IntVarP(&RedisDB, "database", "d", 0, "Redis database. Defaults to 0.")
 	rootCmd.AddCommand(redisRootCmd)
 }
-var ctx = context.Background()
 
+var ctx = context.Background()
 
 var RedisHostname string
 var RedisPort int
 var RedisPassword string
+var RedisDB int
 var redisRootCmd = &cobra.Command{
 	Use:   "redis",
 	Short: "Commands to get redis ",
 	Long:  `Dump all keys and values in Redis.. Safely. Uses SCAN.`,
 }
-
