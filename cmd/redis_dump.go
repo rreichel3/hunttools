5d4f4827ebdc1d34e17ec2d75716c6eb434a67d1
Robert Reichel <rreichel3@github.com>
2021-03-22 12:22:48 -0400

    Working dump with database intelligence

--- cmd/redis_dump.go
+++ cmd/redis_dump.go
@@ -4,6 +4,8 @@ import (
 	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strconv"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/cobra"
@@ -14,6 +16,13 @@ func init() {
 	redisDumpCmd.Flags().BoolVarP(&DumpJson, "json", "j", false, "Dump as json")
 	redisDumpCmd.Flags().BoolVarP(&DumpValues, "values", "", false, "Dump values with keys")
 	redisRootCmd.AddCommand(redisDumpCmd)
+}
+
+type RedisData struct {
+	Hostname string `json:"hostname"`
+	Database int    `json:"database"`
+	Key      string `json:"key"`
+	Value    string `json:"value"`
 }
 
 var MaxDump int
@@ -31,38 +40,82 @@ var redisDumpCmd = &cobra.Command{
 		})
 		var cursor uint64
 		var n int
-		var allKeys = make(map[string]string)
-		for {
-			var keys []string
-			var err error
-			keys, cursor, err = rdb.Scan(ctx, cursor, "*", 10).Result()
-			if err != nil {
-				panic(err)
-			}
-			for _, key := range keys {
-				allKeys[key] = ""
-			}
-			n += len(keys)
-			if cursor == 0 || (MaxDump != -1 && n >= MaxDump) {
-				break
-			}
+
+		keyspace, _ := rdb.Info(ctx, "keyspace").Result()
+		re := regexp.MustCompile(`db(\d+)`)
+		matches := re.FindAllStringSubmatch(keyspace, -1)
+		dbs := make(map[int]bool, 1)
+		for _, match := range matches {
+			dbNum, _ := strconv.Atoi(match[1])
+			dbs[dbNum] = true
 		}
 		if VerboseOutput {
-			fmt.Printf("found %d keys\n", n)
+			fmt.Printf("Dumping %d keyspace(s)..\n", len(dbs))
+			fmt.Printf("%v", dbs)
 		}
-		if DumpValues {
-			for key, _ := range allKeys {
-				val, err := rdb.Dump(ctx, key).Result()
+
+		// A map of databases to keys
+		var allKeys = make(map[int][]string, 0)
+
+		for dbNum, _ := range dbs {
+			rdb := redis.NewClient(&redis.Options{
+				Addr:     fmt.Sprintf("%s:%d", RedisHostname, RedisPort),
+				Password: RedisPassword, // no password set
+				DB:       dbNum,         // use default DB
+			})
+			var keysForDb []string = make([]string, 0)
+
+			for {
+				var keys []string
+				var err error
+				keys, cursor, err = rdb.Scan(ctx, cursor, "*", 10).Result()
 				if err != nil {
-					fmt.Printf("Error encountered getting key: %s\n", key)
-					continue
+					panic(err)
 				}
-				sEnc := b64.StdEncoding.EncodeToString([]byte(val))
-				allKeys[key] = sEnc
+				keysForDb = append(keysForDb, keys...)
+
+				n += len(keys)
+				if cursor == 0 || (MaxDump != -1 && n >= MaxDump) {
+					break
+				}
+			}
+			allKeys[dbNum] = keysForDb
+		}
+
+		if VerboseOutput {
+			fmt.Printf("found %d keys\n", n)
+			fmt.Printf("found %v keys\n", allKeys)
+		}
+
+		if DumpValues {
+			var fullList = make([]RedisData, len(allKeys))
+			for dbNum, keys := range allKeys {
+				rdb = redis.NewClient(&redis.Options{
+					Addr:     fmt.Sprintf("%s:%d", RedisHostname, RedisPort),
+					Password: RedisPassword, // no password set
+					DB:       dbNum,         // use default DB
+				})
+				for _, key := range keys {
+
+					val, err := rdb.Dump(ctx, key).Result()
+					if err != nil {
+						fmt.Printf("Error encountered getting key: %s\n", key)
+						continue
+					}
+					if VerboseOutput {
+						fmt.Printf("Got key: %s\n", key)
+					}
+					sEnc := b64.StdEncoding.EncodeToString([]byte(val))
+					fullList = append(fullList, RedisData{Hostname: RedisHostname, Database: dbNum, Key: key, Value: sEnc})
+				}
 			}
 			idx := 1
 			if DumpJson {
-				jsonStr, _ := json.Marshal(allKeys)
+				jsonStr, err := json.Marshal(fullList)
+				if err != nil {
+					fmt.Printf("Error encountered marshaling json: %s\n", err)
+					return err
+				}
 				fmt.Println(string(jsonStr))
 			} else {
 				for k, v := range allKeys {
@@ -76,11 +129,11 @@ var redisDumpCmd = &cobra.Command{
 			}
 		} else {
 			idx := 1
-			for k, _ := range allKeys {
+			for dbNum, key := range allKeys {
 				if VerboseOutput {
-					fmt.Printf("%d: %s\n", idx, k)
+					fmt.Printf("%d: (DB%d) %s\n", idx, dbNum, key)
 				} else {
-					fmt.Printf("%s\n", k)
+					fmt.Printf("(DB%d) %s\n", dbNum, key)
 				}
 				idx++
 			}
